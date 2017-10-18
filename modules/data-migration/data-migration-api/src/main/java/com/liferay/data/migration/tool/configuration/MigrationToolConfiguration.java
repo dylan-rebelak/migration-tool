de5cac93377bb29f1a594454da281b0fea1209a5
dylan-rebelak <dylan.rebelak@liferay.com>
2017-10-18 10:13:24 -0500

    Add OSGI configuration

---
+++ modules/data-migration/data-migration-api/src/main/java/com/liferay/data/migration/tool/configuration/MigrationToolConfiguration.java
@@ -0,0 +1,29 @@
+package com.liferay.data.migration.tool.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * @author Dylan Rebelak
+ */
+@ExtendedObjectClassDefinition(category = "other")
+
+@Meta.OCD(
+	id = "com.liferay.data.migration.tool.service.configuration.MigrationToolConfiguration"
+)
+public interface MigrationToolConfiguration {
+
+	@Meta.AD(deflt = "4", required = false)
+	public int migrationTaskThreadPoolSize();
+
+	@Meta.AD(deflt = "20", required = false)
+	public int entityServicethreadPoolSize();
+
+	@Meta.AD(deflt = "10", required = false)
+	public int maxBatchQueueSize();
+
+	@Meta.AD(deflt = "200", required = false)
+	public int batchSize();
+
+}
